43de843876aa1e14acb484c25674ce84a55f7bdb
semysong <songye620@naver.com>
2021-11-19 13:49:03 +0900

    "update"

---
+++ happyhouse/src/router.js
@@ -0,0 +1,111 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Index from "./pages/Index.vue";
+import Login from "./pages/Login.vue";
+import Register from "./pages/Register.vue";
+import House from "./pages/House.vue";
+import QnA from "./pages/QnA.vue";
+import CreateForm from "./pages/components/qna/CreateForm.vue";
+import News from "./pages/News.vue";
+import Dust from "./pages/Dust.vue";
+import Chat from "./pages/Chat.vue";
+import MainNavbar from "./layout/MainNavbar.vue";
+import MainFooter from "./layout/MainFooter.vue";
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: "active",
+  routes: [
+    {
+      path: "/",
+      name: "index",
+      components: { default: Index, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/login",
+      name: "login",
+      components: { default: Login, header: MainNavbar },
+      props: {
+        header: { colorOnScroll: 400 },
+      },
+    },
+    {
+      path: "/register",
+      name: "register",
+      components: { default: Register, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/house",
+      name: "house",
+      components: { default: House, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/question",
+      name: "question",
+      components: { default: QnA, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/createquestion",
+      name: "questionForm",
+      components: {
+        default: CreateForm,
+        header: MainNavbar,
+        footer: MainFooter,
+      },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/news",
+      name: "news",
+      components: { default: News, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/dust",
+      name: "dust",
+      components: { default: Dust, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+    {
+      path: "/chat",
+      name: "chat",
+      components: { default: Chat, header: MainNavbar, footer: MainFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: "black" },
+      },
+    },
+  ],
+  scrollBehavior: (to) => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  },
+});
